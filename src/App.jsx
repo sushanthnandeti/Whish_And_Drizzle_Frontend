f55aae6da94ff12abb83ebc47603cfac13ed5583
Sushanth Nandeti <sushanthnandeti96@gmail.com>
2024-10-05 19:30:06 -0400

    Basic Routes Setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
 import { Route, Routes } from "react-router-dom";
 import HomePage from "./pages/HomePage.jsx";
+import SignUpPage from "./pages/SignUpPage.jsx";
+import LoginPage from "./pages/LoginPage.jsx";
 
 function App() {
   
@@ -7,10 +9,10 @@ function App() {
   return (
     <div> 
       <Routes> 
-        
+
         <Route path = '/' element = {<HomePage />} />
-        <Route path = '/' element = {<HomePage />} />
-        <Route path = '/' element = {<HomePage />} />
+        <Route path = '/signup' element = {<SignUpPage />} />
+        <Route path = '/login' element = {<LoginPage />} />
 
        </Routes>
     </div> 
