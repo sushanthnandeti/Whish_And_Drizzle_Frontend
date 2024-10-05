98266f874c2a0dbe0a646b1b188c0e1cafa05e2f
Sushanth Nandeti <sushanthnandeti96@gmail.com>
2024-10-05 19:24:36 -0400

    React Setup

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import { Route, Routes } from "react-router-dom";
+import HomePage from "./pages/HomePage.jsx";
+
+function App() {
+  
+
+  return (
+    <div> 
+      <Routes> 
+        
+        <Route path = '/' element = {<HomePage />} />
+        <Route path = '/' element = {<HomePage />} />
+        <Route path = '/' element = {<HomePage />} />
+
+       </Routes>
+    </div> 
+  );
+}
+
+export default App
